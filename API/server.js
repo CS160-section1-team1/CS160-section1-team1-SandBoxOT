c1117e61fd0d2db70e1bc9be19069d7259fc82a0
Adam Walker <adwalker20@gmail.com>
2021-03-15 14:13:27 -0700

    working on user sign in

--- API/server.js
+++ API/server.js
@@ -1,11 +1,13 @@
 const port = 3000;
 const express = require('express');
 const user_signup = require('./user_signup');
+const user_signin = require('./user_signin');
 
 const app = express();
 app.use(express.json());
 
 // Create new resource
 app.post('/user/signup', user_signup);
+app.post('/user/signin', user_signin);
 
 app.listen(port, () => console.log(`Listening to port ${port}...`));
