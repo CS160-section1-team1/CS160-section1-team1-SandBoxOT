95d171a1ed058dae32cdc9cf78a9802e26d14fe2
Keven Lam <kevenlam2100@gmail.com>
2021-03-20 22:17:46 -0700

    Created package.json and installed EJS

--- controller/API/server.js
+++ controller/API/server.js
@@ -4,9 +4,15 @@ const cors = require('cors');
 const user_signup = require('./user_signup');
 const user_signin = require('./user_signin');
 
+// Register view engine -- Keven Lam
+app.set('view engine', 'ejs');
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+/* Keven Lam */
+
 
 // Create new resource
 app.post('/user/signup', user_signup);
