2a6f597b2918835e83427c9cce80ff7fdb7833ff
Mahdi Khaliki <mahdikhaliki@Mahdis-MacBook-Pro-2.local>
2021-04-17 14:15:56 -0700

    HTTPS configured for API endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,5 @@
 const port = 3000;
 const express = require('express');
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
 const cors = require('cors');
 const user_signup = require('./user_signup');
 const user_signin = require('./user_signin');
@@ -24,10 +21,3 @@ app.post('/signup', user_signup);
 app.post('/signin', user_signin);
 
 app.listen(port, () => console.log(`Listening to port ${port}...`));
-
-const sslServer = https.createServer( {
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app);
-
-sslServer.listen(port, () => console.log(`Secure server on port ${port}`));
