99a9de5f706d716542db22658bd814eb34d505cc
mahdikhaliki <54607305+mahdikhaliki@users.noreply.github.com>
2021-05-05 22:23:11 -0700

    Backend endpoint tests

---
+++ backend/tests/getUserByIdTest.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+function testGetById() {
+	const user_id = 2;
+
+	const URL = `https://api.sandboxot.link/user/${user_id}`;
+
+	const params = {
+		method: "GET",
+		headers: {
+		  'Accept': 'application/json',
+		  'Content-Type':'application/json'
+		},
+		mode: 'cors'
+	};
+
+	fetch(URL, params).then(res => {
+		if(res && res.status == 200)
+			console.log('Get By Id test Passed');
+		else
+			console.log('Get By Id test Failed');
+	});
+}
+
+testGetById();
